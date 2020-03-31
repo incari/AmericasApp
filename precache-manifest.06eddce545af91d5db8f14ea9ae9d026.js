9524059d9fb166503ef66e04c3b57b83c00c276e
Martin Racana <ing.racana@gmail.com>
2020-03-31 17:41:55 +0200

    Updates

---
+++ precache-manifest.06eddce545af91d5db8f14ea9ae9d026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcdd1746f6b9d6ab608306a7901090d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "046810bd82a65042c58f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c98236563c3eeaa460c",
+    "url": "/static/js/2.5d731b2c.chunk.js"
+  },
+  {
+    "revision": "40f08ac5005579b9f3fa1645ca587d26",
+    "url": "/static/js/2.5d731b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046810bd82a65042c58f",
+    "url": "/static/js/main.bff89bbc.chunk.js"
+  },
+  {
+    "revision": "b01e96a8de99bf917786",
+    "url": "/static/js/runtime-main.8a5127f9.js"
+  },
+  {
+    "revision": "37f73bb36cf4742486ddc3b55d67ebf4",
+    "url": "/static/media/Americas-logo.37f73bb3.png"
+  }
+]);
